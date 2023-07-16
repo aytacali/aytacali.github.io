32a7f52df34ec513928c2eaafe1f320b1a0e38ce
Aytac Ali <aytacaliyeva133@gmail.com>
2023-07-16 17:45:45 +0400

    section 2

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -6,7 +6,16 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
-        
+        this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
+		this.camera.position.set( 0, 0, 4 );
+
+		this.scene = new THREE.WebGLRenderer( { antialias: true } );
+		this.renderer.setPixelRatio( window.decivePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		container.appendChild( this.renderer.domElement );
+
+		this.renderer.setAnimationLoop( this.render.bind( this ) );
+
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
@@ -15,7 +24,7 @@ class App{
     }
     
 	render( ) {  
-        
+        this.renderer.render( this.scene, this.camera );
     }
 }
 
